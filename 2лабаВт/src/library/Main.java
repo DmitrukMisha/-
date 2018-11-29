02634469bd8cc781ca1f45481b30886dd0c35622
DmitrukMisha <37069153+DmitrukMisha@users.noreply.github.com>
2018-11-29 23:02:40 +0300

    Update Main.java

--- 2лабаВт/src/library/Main.java
+++ 2лабаВт/src/library/Main.java
@@ -4,7 +4,10 @@ import library.Functions.Registration;
 import library.Interface.Enter;
 import library.Functions.InputFunc;
 
-
+/**
+ * @author Dmitruk M.
+ * @since 20.11.2018
+ */
 public class Main {
 
     public static void main(String[] args) {
